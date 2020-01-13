41dee182d97788ffbc27694351b4f5933d4836ea
Unai Bolivar <ujbolivar@gmail.com>
2020-01-13 17:58:30 +0100

    Fourth commit: Created an episode interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
 import React from "react";
 import { Store } from "./Store";
 
+interface IEpisode {
+  id: number
+url: string
+name: string
+season: number
+number: number
+airdate: string
+airtime: string
+airstamp: string
+runtime: number
+image: {medium: string, original: string}
+summary: string
+}
+
 export default function App(): JSX.Element {
   const { state, dispatch } = React.useContext(Store);
 
